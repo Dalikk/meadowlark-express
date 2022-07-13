fa1aa67990e1bbcc89ecaa0753aafa9d2f92f2f9
Dalikk <dmitriev.alik1999@gmail.com>
2022-07-14 01:27:49 +0900

    code refactor for testing

--- meadowlark.js
+++ meadowlark.js
@@ -1,6 +1,6 @@
+const handlers = require('./lib/handlers')
 const express = require('express')
 const { engine } = require('express-handlebars')
-const fortune = require('./lib/fortune')
 
 const app = express()
 
@@ -13,23 +13,15 @@ app.use(express.static(__dirname + '/public'))
 
 const port = process.env.PORT || 3000
 
-app.get('/', (req, res) => res.render('home'));
+app.get('/', handlers.home);
 
-app.get('/about', (req, res) => {
-    res.render('about', { fortune: fortune.getFortune() });
-});
+app.get('/about', handlers.about);
 
 // Пользовательская страница 404
-app.use((req, res) => {
-    res.status(404);
-    res.render('404');
-});
+app.use(handlers.notFound);
 
 // Пользовательская страница 500
-app.use((req, res) => {
-    res.status(500);
-    res.render('500');
-})
+app.use(handlers.serverError)
 
 app.listen(port, () => console.log(
     `Express запущен на http://localhost:${port} ` +
