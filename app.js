7b6c382d0cd6afbbe7251ca8f5715dc15c7e55c1
Dalikk <dmitriev.alik1999@gmail.com>
2022-07-15 00:58:10 +0900

    file uploading AJAX

--- app.js
+++ app.js
@@ -39,6 +39,7 @@ app.get('/newsletter', handlers.newsletter);
 app.post('/api/newsletter-signup', handlers.api.newsletterSignup);
 
 app.get('/contest/vacation-photo', handlers.vacationPhotoContest);
+app.get('/contest/vacation-photo-ajax', handlers.vacationPhotoContestAjax);
 app.get('/contest/vacation-photo-thank-you', handlers.vacationPhotoContestThankYou);
 
 app.post('/contest/vacation-photo/:year/:month', (req, res) => {
@@ -48,6 +49,13 @@ app.post('/contest/vacation-photo/:year/:month', (req, res) => {
     console.log('got fields: ', fields);
     console.log('and files: ', files);
     handlers.vacationPhotoContestProcess(req, res, fields, files);
+  });
+});
+app.post('/api/vacation-photo-contest/:year/:month', (req, res) => {
+  const form = new multiparty.Form();
+  form.parse(req, (err, fields, files) => {
+    if(err) return handlers.api.vacationPhotoContestError(req, res, err.message);
+    handlers.api.vacationPhotoContest(req, res, fields, files);
   });
 });
 
