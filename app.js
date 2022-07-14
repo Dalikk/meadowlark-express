cf9a6a90bc864c047bd1a922c35e46c0703ef156
Dalikk <dmitriev.alik1999@gmail.com>
2022-07-14 22:50:21 +0900

    add AJAX form handling

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const { create } = require('express-handlebars');
 
 const app = express();
 app.use(bodyParser.urlencoded({ extend: true }));
+app.use(bodyParser.json());
 
 const hbs = create({
   helpers: {
@@ -30,10 +31,11 @@ app.get('/', handlers.home);
 app.get('/about', handlers.about);
 
 app.get('/newsletter-signup', handlers.newsletterSignup);
-
 app.post('/newsletter-signup/process', handlers.newsletterSignupProcess);
-
 app.get('/newsletter-signup/thank-you', handlers.newsletterSignupThankYou);
+
+app.get('/newsletter', handlers.newsletter);
+app.post('/api/newsletter-signup', handlers.api.newsletterSignup);
 
 // Пользовательская страница 404
 app.use(handlers.notFound);
