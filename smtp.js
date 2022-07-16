5ec7967d0d3d7eb0f3041fd3a32dbb735a273574
Dalikk <dmitriev.alik1999@gmail.com>
2022-07-17 00:40:24 +0900

    multiple recipient email send

--- smtp.js
+++ smtp.js
@@ -15,9 +15,9 @@ const go = async () => {
   try {
     const result = await mailTransport.sendMail({
       from: '"Meadowlark Travel" <dalikk1@mail.ru>',
-      to: 'dmitriev.alik1999@gmail.com',
+      to: 'dmitriev.alik1999@gmail.com, instrumenty.ykt@gmail.com',
       subject: 'Your Meadowlark Travel Tour',
-      text: 'Thank you for booking your trip with Meadowlark Travel.  ' +
+      text: 'Test Mail.  ' +
         'We look forward to your visit!',
     });
     console.log('mail sent successfully: ', result);
