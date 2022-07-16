3c7b20370b76024dce51738f8fbac246cb19a45f
Dalikk <dmitriev.alik1999@gmail.com>
2022-07-17 00:36:27 +0900

    single recipient email send

---
+++ smtp.js
@@ -0,0 +1,29 @@
+const nodemailer = require('nodemailer');
+
+const credential = require('./credentias');
+
+const mailTransport = nodemailer.createTransport({
+  port: 465,
+  host: 'smtp.mail.ru',
+  auth: {
+    user: credential.sendgrid.user,
+    pass: credential.sendgrid.password,
+  },
+});
+
+const go = async () => {
+  try {
+    const result = await mailTransport.sendMail({
+      from: '"Meadowlark Travel" <dalikk1@mail.ru>',
+      to: 'dmitriev.alik1999@gmail.com',
+      subject: 'Your Meadowlark Travel Tour',
+      text: 'Thank you for booking your trip with Meadowlark Travel.  ' +
+        'We look forward to your visit!',
+    });
+    console.log('mail sent successfully: ', result);
+  } catch (err) {
+    console.log('could not send email: ' + err.message);
+  }
+};
+
+go();
